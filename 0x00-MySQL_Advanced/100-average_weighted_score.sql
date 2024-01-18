c476589e306b7f04c68bfcb5aeffd1dc768efb93
pytbyte <heretolearn1@gmail.com>
2024-01-18 23:58:55 +0300

    Task 12

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,33 +1,33 @@
--- stored procedure ComputeAverageWeightedScoreForUser
+-- Create a stored procedure ComputeAverageWeightedScoreForUser
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
-    IN user_id INT
+    IN p_user_id INT
 )
 BEGIN
     DECLARE total_weighted_score FLOAT;
     DECLARE total_weight FLOAT;
     
-    -- variables
+    -- Initialize variables
     SET total_weighted_score = 0;
     SET total_weight = 0;
 
-    --weighted score
+    -- Calculate the total weighted score
     SELECT SUM(corrections.score * projects.weight) INTO total_weighted_score
     FROM corrections
     JOIN projects ON corrections.project_id = projects.id
-    WHERE corrections.user_id = user_id;
+    WHERE corrections.user_id = p_user_id;
 
-    -- total weight
+    -- Calculate the total weight
     SELECT SUM(weight) INTO total_weight
     FROM projects;
 
-    -- Update the average_score 
+    -- Update the average_score for the user
     UPDATE users
     SET average_score = IFNULL(total_weighted_score / total_weight, 0)
-    WHERE id = user_id;
+    WHERE id = p_user_id;
 
 END;
 //
