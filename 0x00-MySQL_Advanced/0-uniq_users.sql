bca247f64fb583c6dd934aa7d952f2806f41c01d
pytbyte <heretolearn1@gmail.com>
2024-01-19 01:04:06 +0300

    final push

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
--- Create the 'users' table if it does not exist
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- Create the users table if it does not exist
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
-)
+);
