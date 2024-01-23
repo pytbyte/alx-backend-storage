5e34b52effc5a202de3cf72a2307a8e8b9ce18f1
pytbyte <heretolearn1@gmail.com>
2024-01-23 21:18:11 +0300

    top student #! added

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python3
-'''top_students module.
+'''print_nginx_request_logs module.
 '''
-#!/usr/bin/env python3
-'''Task 14's module.
-'''
-
-
 def top_students(mongo_collection):
-    '''Prints all students in a collection sorted by average score.
+    '''Returns all students in a collection sorted by average score.
     '''
-    students = mongo_collection.aggregate(
-        [
-            {
-                '$project': {
-                    '_id': 1,
-                    'name': 1,
-                    'averageScore': {
-                        '$avg': {
-                            '$avg': '$topics.score',
-                        },
-                    },
-                    'topics': 1,
+    # Define the aggregation pipeline
+    aggregation_pipeline = [
+        {
+            '$project': {
+                '_id': 1,
+                'name': 1,
+                'averageScore': {
+                    '$avg': '$topics.score',
                 },
             },
-            {
-                '$sort': {'averageScore': -1},
-            },
-        ]
-    )
-    return students
+        },
+        {
+            '$sort': {'averageScore': -1},
+        },
+    ]
+
+    # Execute the aggregation pipeline
+    top_students_cursor = mongo_collection.aggregate(aggregation_pipeline)
+
+    # Convert the cursor to a list and return the result
+    top_students_list = list(top_students_cursor)
+    return top_students_list
