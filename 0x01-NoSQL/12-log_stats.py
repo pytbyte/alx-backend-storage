1c4c7234961d03fa747420966a1fbcb42abe4dbc
pytbyte <heretolearn1@gmail.com>
2024-01-23 21:11:19 +0300

    log_stat

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,27 +3,23 @@
 '''
 from pymongo import MongoClient
 
+
 def print_nginx_request_logs():
     '''Prints stats about Nginx request logs.
     '''
-    # Connect to MongoDB
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
-
-    # Print total logs count
-    total_logs_count = nginx_collection.count_documents({})
-    print(f'{total_logs_count} logs')
-
-    # Print request methods count
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
-    for method in methods:
-        req_count = nginx_collection.count_documents({'method': method})
-        print(f'\tmethod {method}: {req_count}')
+    all_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in all_methods:
+        requsts_ = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, requsts_))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(status_checks_count))
 
-    # Print status checks count
-    status_checks_count = nginx_collection.count_documents({'method': 'GET', 'path': '/status'})
-    print(f'{status_checks_count} status check')
 
 if __name__ == '__main__':
     print_nginx_request_logs()
