bfa52a18f16ee9d414809ca9b4cbf89c5690a47f
pytbyte <heretolearn1@gmail.com>
2024-01-25 05:23:33 +0300

    REDIS CACHE

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,44 +1,38 @@
+#!/usr/bin/env python3
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
+
 redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
 def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
-    def invoker(url: str) -> str:
-        # Increment the count for the URL
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
         redis_store.incr(f'count:{url}')
-        
-        # Try to retrieve the cached result
         result = redis_store.get(f'result:{url}')
-        
         if result:
-            # If cached result exists, return it
             return result.decode('utf-8')
-        
-        # If not cached, fetch the data using the original function
         result = method(url)
-        
-        # Set the result in the cache with a TTL of 10 seconds
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
-        
         return result
-
     return invoker
 
 
 @data_cacher
 def get_page(url: str) -> str:
-    # Fetch the HTML content of the URL using requests
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        # Return the content if the request was successful
-        return response.text
-    else:
-        # Handle other status codes if needed
-        return f"Error: {response.status_code}"
-
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
